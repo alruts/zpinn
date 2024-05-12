27a72f987fae0ab363026b00ec99ea28c6355f53
stnj <stnj@demant.com>
2024-05-12 16:15:10 +0200

    fix rmk+1 model bug

--- src/zpinn/impedance_models.py
+++ src/zpinn/impedance_models.py
@@ -8,9 +8,9 @@ def constant_impedance(coeffs, f):
 
 def RMK_plus_1(coeffs, f):
     """Return the impedance of the RMK+1 model at a given frequency."""
-    w = 2 * jnp.pi * f
-    z = coeffs["K"] * (1j * w) ** -1
+    omega = 2 * jnp.pi * f
+    z = coeffs["K"] * (1j * omega) ** -1
     z += coeffs["R_1"]
-    Z += coeffs["M"] * (1j * w)
-    Z += coeffs["G"] * (1j * w) ** coeffs["gamma"]
-    return z
+    z += coeffs["M"] * (1j * omega)
+    z += coeffs["G"] * (1j * omega) ** coeffs["gamma"]
+    return z.real, z.imag
