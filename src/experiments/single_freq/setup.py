7b0096c147d8af1b3c9dabb9dde172386974a49f
stnj <stnj@demant.com>
2024-05-14 18:13:32 +0200

    update setup file

--- src/experiments/single_freq/setup.py
+++ src/experiments/single_freq/setup.py
@@ -1,91 +1,32 @@
-from zpinn.dataio import DomainSampler, PressureDataset
-from omegaconf import OmegaConf
+from zpinn.get_loaders import get_loaders
 import optax
 
-config = OmegaConf.load("conf/experiments/single_freq/config.yaml")
+
+def setup_loaders(config, restrict_to=None):
+    return get_loaders(config, restrict_to=config.batch.data.restrict_to)
 
 
-def data_handlers(config, restrict_to=None):
-
-    # extract domain and boundary configurations
-    domain = config.collocation.domain
-    boundary = config.collocation.boundary
-
-    # create dataset
-    dataset = PressureDataset(
-        path=config.DATASET.path,
-    )
-
-    if restrict_to is not None:
-        dataset.restrict_to(restrict_to)
-
-    # domain points
-    n_dom = config.DOMAIN.num_points
-    dom_limits = {
-        "x": (domain.x.min, domain.x.max),
-        "y": (domain.y.min, domain.y.max),
-        "z": (domain.z.min, domain.z.max),
-        "f": (domain.f.min, domain.f.max),
-    }
-    dom_distr = {
-        "x": domain.x.distribution,
-        "y": domain.y.distribution,
-        "z": domain.z.distribution,
-        "f": domain.f.distribution,
-    }
-    # boundary points
-    n_bnd = config.BOUNDARY.num_points
-    bnd_limits = {
-        "x": (boundary.x.min, boundary.x.max),
-        "y": (boundary.y.min, boundary.y.max),
-        "z": (boundary.z.min, boundary.z.max),
-        "f": (boundary.f.min, boundary.f.max),
-    }
-    bnd_distr = {
-        "x": boundary.x.distribution,
-        "y": boundary.y.distribution,
-        "z": boundary.z.distribution,
-        "f": boundary.f.distribution,
-    }
-    # create domain generator
-    dom_gen = DomainSampler(
-        n_points=n_dom,
-        limits=dom_limits,
-        transforms=dataset.transforms,
-        distributions=dom_distr,
-    )
-    # create boundary generator
-    bnd_gen = DomainSampler(
-        n_points=n_bnd,
-        limits=bnd_limits,
-        transforms=dataset.transforms,
-        distributions=bnd_distr,
-    )
-
-    # create eval generator
-    eval_gen = bnd_gen
-    eval_gen.n_points = int(100**2)
-
-    return dataset, dom_gen, bnd_gen, eval_gen
-
-
-def optimizers(config):
-    # Define optimizerss
-    optimizer_lamdas = optax.adam(learning_rate=config.train.learning_rate.weights)
-    optimizer_coeffs = optax.sgd(learning_rate=config.train.learning_rate.coeffs)
-
-    scheduler = optax.join_schedules(
+def setup_optimizers(config):
+    # lr schedule
+    lr = optax.join_schedules(
         schedules=[
-            optax.constant_schedule(config.train.learning_rate.model),
+            optax.constant_schedule(config.training.learning_rate.params),
             optax.cosine_decay_schedule(
-                config.train.learning_rate.model, config.train.steps // 2
+                config.training.learning_rate.params, config.training.steps // 2
             ),
         ],
-        boundaries=[config.train.steps // 2],
+        boundaries=[config.training.steps // 2],
+    )
+    optimizer_params = optax.chain(
+        optax.clip(1.0),
+        optax.adam(learning_rate=lr),
+    )
+    optimizer_coeffs = optax.chain(
+        optax.adam(learning_rate=config.training.learning_rate.coeffs),
+    )
+    optimizers = dict(
+        params=optimizer_params,
+        coeffs=optimizer_coeffs,
     )
 
-    optimizer_model = optax.chain(
-        optax.clip(1.0),
-        optax.adam(learning_rate=scheduler),
-    )
-    return scheduler, optimizer_model, optimizer_lamdas, optimizer_coeffs
+    return lr, optimizers
