b72dd2e42dedf6cd65e8c6c030f93d64b82e5ab7
stnj <stnj@demant.com>
2024-05-14 14:58:38 +0200

    update preprocess script

--- src/data_gen/preprocess_dataset.py
+++ src/data_gen/preprocess_dataset.py
@@ -1,41 +1,40 @@
 import logging
 import os
-import sys
 
-import hydra
-import pandas as pd
-import dill
+from omegaconf import OmegaConf
 import numpy as np
+import pandas as pd
+import argparse
 
-from ..zpinn.constants import _c0, _rho0
 logging.basicConfig(level=logging.INFO)
 
-
-def nondim(x, x0=None, xc=None):
-    """Normalize the data"""
-    if x0:
-        x -= x0
-    if xc:
-        x /= xc
-    return x
-
-
-def downsample(data, factor):
-    """Downsample the data"""
-    return data[::factor, ::factor, :]
-
-
-@hydra.main(
-    config_path="../../conf", config_name="inf_baffle", version_base=hydra.__version__
+# parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--config_path",
+    type=str,
+    default="src\data_gen\conf\inf_baffle.yaml",
+    help="path to the config file",
 )
-def preprocess(cfg):
+parser.add_argument(
+    "-d", "--downsample_factor", type=int, default=1, help="downsample factor"
+)
+args = parser.parse_args()
+
+# Load the config
+CONFIG = OmegaConf.load(os.path.join(args.config_path))
+
+# Helper function
+downsample_fn = lambda data, factor: data[::factor, ::factor, :]
+
+def preprocess(cfg=CONFIG):
     raw_path = os.path.join(
-        cfg.dataset.out_dir,
+        cfg.paths.data,
         "raw",
         f"{cfg.dataset.name}.pkl",
     )
     processed_path = os.path.join(
-        cfg.dataset.out_dir,
+        cfg.paths.data,
         "processed",
         f"{cfg.dataset.name}.pkl",
     )
@@ -46,138 +45,98 @@ def preprocess(cfg):
     # Initialize processed DataFrame
     processed_df = pd.DataFrame()
 
-    downsample_factor = 19
-
     # concatenate all pressure values to compute mean and max
     _p_re = np.concatenate(
-        [raw_df[frequency].pressure.real for frequency in cfg.dataset.frequencies],
+        [raw_df[frequency]["real_pressure"] for frequency in cfg.dataset.frequencies],
         axis=-1,
     )
     _p_im = np.concatenate(
-        [raw_df[frequency].pressure.imag for frequency in cfg.dataset.frequencies],
+        [raw_df[frequency]["imag_pressure"] for frequency in cfg.dataset.frequencies],
         axis=-1,
     )
 
     # Inference grid from the first frequency
-    x, y, z = raw_df[cfg.dataset.frequencies[0]].grid
+    x, y, z = raw_df.attrs["grid"]
 
     # Get frequencies
     f = np.array(cfg.dataset.frequencies, dtype=np.float32)
 
     # find constants for transforming the data
-    p_re0 = np.mean(_p_re)
-    p_im0 = np.mean(_p_im)
+    a0 = np.mean(_p_re)
+    b0 = np.mean(_p_im)
     x0 = np.mean([cfg.postprocessing.x.min, cfg.postprocessing.x.max])
     y0 = np.mean([cfg.postprocessing.y.min, cfg.postprocessing.y.max])
     z0 = np.mean([cfg.postprocessing.z.min, cfg.postprocessing.z.max])
     f0 = np.mean([cfg.postprocessing.f.min, cfg.postprocessing.f.max])
 
     # find the max difference frvom the mean to norm in [-1, 1]
-    p_rec = np.max(np.abs(_p_re - p_re0))
-    p_imc = np.max(np.abs(_p_im - p_im0))
+    ac = np.std(np.abs(_p_re - a0))
+    bc = np.std(np.abs(_p_im - b0))
     xc = np.max(np.abs(x - x0))
     yc = np.max(np.abs(y - y0))
     zc = np.max(np.abs(z - z0))
     fc = np.max(np.abs(f - f0))
 
-    # non dimensionalize the 4D grid
-    f = nondim(f, f0, fc)
-    x = nondim(x, x0, xc)
-    y = nondim(y, y0, yc)
-
-    if not cfg.postprocessing.z.nondim:
-        z0, zc = 0.0, 1.0
-        z = nondim(z, z0, zc)
-    else:
-        z = nondim(z, z0, zc)
-
-    # downsample x, y
-    x, y, z = [downsample(arr, downsample_factor) for arr in (x, y, z)]
+    # downsample spatial coordinates
+    x, y, z = [downsample_fn(arr, args.downsample_factor) for arr in (x, y, z)]
 
     # save the transforms
     transforms = {
-        "x": (x0, xc),
-        "y": (y0, yc),
-        "z": (z0, zc),
-        "f": (f0, fc),
-        "real_pressure": (p_re0, p_rec),
-        "imag_pressure": (p_im0, p_imc),
+        "x0": x0,
+        "xc": xc,
+        "y0": y0,
+        "yc": yc,
+        "z0": z0,
+        "zc": zc,
+        "f0": f0,
+        "fc": fc,
+        "a0": a0,
+        "ac": ac,
+        "b0": bc,
+        "bc": b0,
     }
 
     # loop over the frequencies and save the ground truth values
-    for ii, frequency in enumerate(cfg.dataset.frequencies):
+    for idx, frequency in enumerate(cfg.dataset.frequencies):
         # Load the data
         data = raw_df[frequency]
 
         # Unpack the data
-        p_re = data.pressure.real
-        p_im = data.pressure.imag
-        Zn = data.impedance
-
-        # transform the data
-        p_re = nondim(p_re, p_re0, p_rec)
-        p_im = nondim(p_im, p_im0, p_imc)
+        p_re = data["real_pressure"]
+        p_im = data["imag_pressure"]
 
         # downsample the data
-        p_re = downsample(p_re, downsample_factor)
-        p_im = downsample(p_im, downsample_factor)
+        p_re = downsample_fn(p_re, args.downsample_factor)
+        p_im = downsample_fn(p_im, args.downsample_factor)
 
         # save ground truth and transforms to the processed dataframe
         gt = {
             "real_pressure": p_re,
             "imag_pressure": p_im,
-            "real_impedance": Zn.real,
-            "imag_impedance": Zn.imag,
+            "real_impedance": data["real_impedance"],
+            "imag_impedance": data["imag_impedance"],
+            "ref": data.ref,
         }
 
         # save gt and transforms to the processed dataframe
-        processed_df[f[ii]] = gt
-
-    logging.info(f"Saving the dataframe to {processed_path}")
+        processed_df[f[idx]] = gt
 
     # save attrs
     processed_df.attrs = {
         "name": cfg.dataset.name,
-        "downsample_factor": downsample_factor,
+        "downsample_factor": args.downsample_factor,
         "transforms": transforms,
         "grid": (x, y, z, f),
     }
 
     # Save the DataFrame
+    logging.info(f"Saving the dataframe to {processed_path}")
     processed_df.to_pickle(processed_path)
 
-    # print f bar
-    logging.log(logging.INFO, f"f bar: {f}")
+    # log output shape
+    logging.info(f"Output shape: {p_re.shape}")
 
-    # log output shapes
-    logging.log(logging.INFO, f"p_re shape: {p_re.shape}")
-    logging.log(logging.INFO, f"p_im shape: {p_im.shape}")
-    logging.log(logging.INFO, f"x shape: {x.shape}")
-    logging.log(logging.INFO, f"y shape: {y.shape}")
-    logging.log(logging.INFO, f"z shape: {z.shape}")
-
-    # log mean and variance of the processed data
-    logging.log(
-        logging.INFO, f"Mean real pressure: {np.mean(p_re)}, var: {np.var(p_re)}"
-    )
-    logging.log(
-        logging.INFO, f"Mean imag pressure: {np.mean(p_im)}, var: {np.var(p_im)}"
-    )
-    logging.log(logging.INFO, f"Mean x: {np.mean(x)}, var: {np.var(x)}")
-    logging.log(logging.INFO, f"Mean y: {np.mean(y)}, var: {np.var(y)}")
-    logging.log(logging.INFO, f"Mean z: {np.mean(z)}, var: {np.var(z)}")
-    logging.log(logging.INFO, f"Mean f: {np.mean(f)}, var: {np.var(f)}")
-
-    # log min and max of the processed data
-    logging.log(logging.INFO, f"Min real pressure: {np.min(p_re)}, max: {np.max(p_re)}")
-    logging.log(logging.INFO, f"Min imag pressure: {np.min(p_im)}, max: {np.max(p_im)}")
-    logging.log(logging.INFO, f"Min x: {np.min(x)}, max: {np.max(x)}")
-    logging.log(logging.INFO, f"Min y: {np.min(y)}, max: {np.max(y)}")
-    logging.log(logging.INFO, f"Min z: {np.min(z)}, max: {np.max(z)}")
-    logging.log(logging.INFO, f"Min f: {np.min(f)}, max: {np.max(f)}")
-
-    print(processed_df.head())
-
+    print("Transforms: ", transforms)
 
 if __name__ == "__main__":
     preprocess()
