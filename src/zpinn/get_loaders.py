6638d4c49718830aa59e252bef5db69bef081d2b
stnj <stnj@demant.com>
2024-05-14 18:14:15 +0200

    add data filtering

--- src/zpinn/get_loaders.py
+++ src/zpinn/get_loaders.py
@@ -4,11 +4,15 @@ sys.path.append("src")
 from zpinn.dataio import BoundarySampler, DomainSampler, PressureDataset
 
 
-def get_loaders(config, custom_transforms=None):
+def get_loaders(config, custom_transforms=None, restrict_to=None):
     dataset = PressureDataset(config.paths.dataset)
 
     if custom_transforms is not None:
         dataset.transforms = custom_transforms
+
+    if restrict_to is not None:
+        print(f"Restricting dataset to {restrict_to}")
+        dataset.restrict_to(**restrict_to)
 
     dataloader = dataset.get_dataloader(
         batch_size=config.batch.data.batch_size, shuffle=True
