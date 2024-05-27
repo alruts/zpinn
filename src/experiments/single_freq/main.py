bf837996390dcfba5a20ebf0ad98367788cc7788
stnj <stnj@demant.com>
2024-05-27 10:22:44 +0200

    use hydra for config management

--- src/experiments/single_freq/main.py
+++ src/experiments/single_freq/main.py
@@ -5,18 +5,10 @@ import hydra
 import jax
 import train
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "-c",
-    "--config_file",
-    type=str,
-    default="default.yaml",
-    help="Path to the config file",
-)
-args = parser.parse_args()
-
 @hydra.main(
-    config_path=".\configs", config_name="default", version_base=hydra.__version__
+    config_path=".\configs",
+    config_name="default",
+    version_base=hydra.__version__,
 )
 def main(config):
 
@@ -25,5 +17,5 @@ def main(config):
 
 
 if __name__ == "__main__":
-    
+
     main()
