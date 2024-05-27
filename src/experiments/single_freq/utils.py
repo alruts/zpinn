cd0b0a504f802490abd4621cd8fd8402a86910ab
stnj <stnj@demant.com>
2024-05-27 10:24:20 +0200

    update optimizer settings.

--- src/experiments/single_freq/utils.py
+++ src/experiments/single_freq/utils.py
@@ -7,26 +7,37 @@ def setup_loaders(config):
 
 
 def setup_optimizers(config):
-    # lr schedule
-    lr = optax.join_schedules(
-        schedules=[
-            optax.constant_schedule(config.training.learning_rate.params),
-            optax.cosine_decay_schedule(
-                config.training.learning_rate.params, config.training.steps // 2
-            ),
-        ],
-        boundaries=[config.training.steps // 2],
+    lr = optax.exponential_decay(
+        init_value=config.training.optim.params.lr,
+        transition_steps=config.training.optim.params.transition_steps,
+        decay_rate=config.training.optim.params.decay_rate,
     )
+
     optimizer_params = optax.chain(
         optax.clip(1.0),
         optax.adam(learning_rate=lr),
     )
+
+    # lr = optax.cosine_onecycle_schedule(
+    #     transition_steps=config.training.optim.coeffs.transition_steps,
+    #     peak_value=config.training.optim.coeffs.lr,
+    #     pct_start=config.training.optim.coeffs.pct_start,
+    # )
+    
+    lr = config.training.optim.coeffs.lr
     optimizer_coeffs = optax.chain(
-        optax.adam(learning_rate=config.training.learning_rate.coeffs),
+        optax.adam(learning_rate=lr),
     )
+
     optimizers = dict(
         params=optimizer_params,
         coeffs=optimizer_coeffs,
     )
+    
+    if config.weighting.scheme == "mle":
+        optimizer_weights = optax.chain(
+            optax.adam(learning_rate=config.training.optim.weights.lr),
+        )
+        optimizers["weights"] = optimizer_weights
 
-    return lr, optimizers
+    return optimizers
