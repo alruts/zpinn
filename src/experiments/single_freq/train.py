20c42e93b611f59dcedfddb03c2bbfb18aa560e7
stnj <stnj@demant.com>
2024-05-31 16:17:55 +0200

    add loss transition

--- src/experiments/single_freq/train.py
+++ src/experiments/single_freq/train.py
@@ -2,27 +2,25 @@ import logging
 import os
 import sys
 
+# Block GPU from tensorflow
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
 import equinox as eqx
 import hydra
 import jax.random as jrandom
-from jax.numpy import save, load
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
-import pickle
 
 sys.path.append("src")
 from experiments.single_freq.utils import setup_loaders, setup_optimizers
-from zpinn.models.BVPModel import BVPModel
 from zpinn.models.BVPEvaluator import BVPEvaluator
+from zpinn.models.BVPModel import BVPModel
 from zpinn.models.ModifiedSIREN import ModifiedSIREN
 from zpinn.models.SIREN import SIREN
 
 
 def train_and_evaluate(config):
-    time_stamp = hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
-    head, time = os.path.split(time_stamp)
-    _, date = os.path.split(head)
-    writer_path = os.path.join(config.paths.log_dir, date, time)
+    writer_path = hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
     writer = SummaryWriter(writer_path)
 
     logging.info(f"Logging to {writer_path}")
@@ -38,8 +36,6 @@ def train_and_evaluate(config):
         model = ModifiedSIREN(**config.architecture, key=subkey)
     else:
         raise ValueError(f"Invalid architecture: {config.architecture}")
-    
-    
 
     # data iterators
     dataloader, dom_sampler, bnd_sampler, ref_coords, ref_gt, transforms = (
@@ -70,7 +66,9 @@ def train_and_evaluate(config):
     if config.weighting.scheme == "mle":
         opt_states["weights"] = optimizers["weights"].init(bvp.weights)
 
-    print("Starting training, waiting JIT compilation...")
+    
+    # ------------------- Training -------------------
+    logging.info("Starting training, wait for JIT compilation...")
     for step in tqdm(range(config.training.steps)):
         # load batch
         batch = dict(
@@ -98,6 +96,23 @@ def train_and_evaluate(config):
         if step % config.logging.log_interval == 0:
             evaluator(params, coeffs, weights, batch, step, ref_coords, ref_gt)
 
+        # transition to boundary loss
+        if (
+            step == config.weighting.transition_step
+            and config.weighting.transition_step is not None
+        ):
+            assert (
+                config.weighting.scheme == "grad_norm"
+            ), "Only grad_norm supported for transition to boundary loss."
+            
+            config.weighting.use_boundary_loss = True  # switch to boundary loss
+            bvp = BVPModel(model, transforms, config, params, None, coeffs)  # reset bvp
+            weights = bvp.weights  # reset weights
+            new_w = bvp.compute_weights(params, coeffs, **batch)
+            weights = bvp.update_weights(weights, new_w)
+            
+    # -----------------------------------------------
+
     # Save model in hydra output directory
     model_path = (
         hydra.core.hydra_config.HydraConfig.get().runtime.output_dir + "/model.eqx"
@@ -105,5 +120,5 @@ def train_and_evaluate(config):
     bvp = BVPModel(model, transforms, config, params, None, coeffs)
     eqx.tree_serialise_leaves(model_path, bvp)
     logging.info(f"Model saved to {model_path}")
-    
+
     writer.close()
