48b06b793bbf8a34c5c956163c4ca12bd4cbc0c2
stnj <stnj@demant.com>
2024-05-31 16:17:17 +0200

    simplify optimizer for coeffs

--- src/experiments/single_freq/utils.py
+++ src/experiments/single_freq/utils.py
@@ -18,22 +18,16 @@ def setup_optimizers(config):
         optax.adam(learning_rate=lr),
     )
 
-    # lr = optax.cosine_onecycle_schedule(
-    #     transition_steps=config.training.optim.coeffs.transition_steps,
-    #     peak_value=config.training.optim.coeffs.lr,
-    #     pct_start=config.training.optim.coeffs.pct_start,
-    # )
-    
-    lr = config.training.optim.coeffs.lr
     optimizer_coeffs = optax.chain(
-        optax.adam(learning_rate=lr),
+        optax.clip(1.0),
+        optax.adam(learning_rate=config.training.optim.coeffs.lr),
     )
 
     optimizers = dict(
         params=optimizer_params,
         coeffs=optimizer_coeffs,
     )
-    
+
     if config.weighting.scheme == "mle":
         optimizer_weights = optax.chain(
             optax.adam(learning_rate=config.training.optim.weights.lr),
