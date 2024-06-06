ae1673cb0a7cbc3a32f2a3cbf98a26e13fad37d2
stnj <stnj@demant.com>
2024-06-06 17:39:33 +0200

    add pirate net and refactor bnd loss transition

--- src/experiments/domain_3d/train.py
+++ src/experiments/domain_3d/train.py
@@ -13,7 +13,30 @@ from experiments.domain_3d.utils import setup_loaders, setup_optimizers
 from zpinn.models.BVPEvaluator import BVPEvaluator
 from zpinn.models.BVPModel import BVPModel
 from zpinn.models.ModifiedSIREN import ModifiedSIREN
+from zpinn.models.PirateSIREN import PirateSIREN
 from zpinn.models.SIREN import SIREN
+
+
+def transition_to_boundary_loss(
+    config, model, transforms, params, coeffs, evaluator, batch
+):
+    assert (
+        config.weighting.scheme == "grad_norm"
+    ), "Only grad_norm supported for transition to boundary loss."
+
+    # switch to boundary loss
+    config.weighting.use_boundary_loss = True
+    bvp = BVPModel(model, transforms, config, params, None, coeffs)  # reset bvp
+
+    # update evaluator
+    evaluator.bvp = bvp
+
+    # recalculate weights
+    weights = bvp.weights
+    new_w = bvp.compute_weights(params, coeffs, **batch, argnums=0)
+    weights = bvp.update_weights(weights, new_w)
+
+    return bvp, weights
 
 
 def train_and_evaluate(config):
@@ -31,6 +54,8 @@ def train_and_evaluate(config):
         model = SIREN(**config.architecture, key=subkey)
     elif config.architecture.name == "modified_siren":
         model = ModifiedSIREN(**config.architecture, key=subkey)
+    elif config.architecture.name == "pirate_siren":
+        model = PirateSIREN(**config.architecture, key=subkey)
     else:
         raise ValueError(f"Invalid architecture: {config.architecture}")
 
@@ -40,7 +65,7 @@ def train_and_evaluate(config):
     )
 
     # bvp
-    bvp = BVPModel(model, transforms, config)
+    bvp = BVPModel(model, config, transforms)
     evaluator = BVPEvaluator(bvp, writer, config)
 
     # load initial model if provided
@@ -63,35 +88,24 @@ def train_and_evaluate(config):
     if config.weighting.scheme == "mle":
         opt_states["weights"] = optimizers["weights"].init(bvp.weights)
 
-    # ------------------- Training -------------------
+    # ------------------- Training loop -------------------
     logging.info("Starting training, wait for JIT compilation...")
     for step in tqdm(range(config.training.steps)):
-        # load batch
+
+        if (
+            step == config.weighting.transition_step
+            and config.weighting.transition_step is not None
+        ):
+            bvp, weights = transition_to_boundary_loss(
+                config, model, transforms, params, coeffs, evaluator, batch
+            )
+            
         batch = dict(
             dat_batch=next(iter(dataloader)),
             dom_batch=next(iter(dom_sampler)),
             bnd_batch=next(iter(bnd_sampler)),
         )
-        
-        # transition to boundary loss
-        if (
-            step == config.weighting.transition_step
-            and config.weighting.transition_step is not None
-        ):
-            assert (
-                config.weighting.scheme == "grad_norm"
-            ), "Only grad_norm supported for transition to boundary loss."
-            
-            config.weighting.use_boundary_loss = True  # switch to boundary loss
-            bvp = BVPModel(model, transforms, config, params, None, coeffs)  # reset bvp
-            evaluator.bvp = bvp  # reset evaluator
-            # reset weights
-            weights = bvp.weights  
-            new_w = bvp.compute_weights(params, coeffs, **batch, argnums=0)
-            weights = bvp.update_weights(weights, new_w)
 
-
-        # step
         if config.weighting.scheme == "grad_norm":
             params, coeffs, opt_states = bvp.update(
                 params, weights, coeffs, opt_states, optimizers, batch
@@ -100,30 +114,25 @@ def train_and_evaluate(config):
             if step % config.weighting.update_every == 0:
                 new_w = bvp.compute_weights(params, coeffs, **batch, argnums=0)
                 weights = bvp.update_weights(weights, new_w)
-                
-                # new_w = bvp.compute_weights(params, coeffs, **batch, argnums=1)
-                # c_weights = bvp.update_weights(weights, new_w)
 
         if config.weighting.scheme == "mle":
             params, weights, coeffs, opt_states = bvp.update(
                 params, weights, coeffs, opt_states, optimizers, batch
             )
-            
-            # weight clipping
+            # TODO: add this to config
             weights["bc_im"] = jnp.maximum(weights["bc_im"], 1.0)
             weights["bc_re"] = jnp.maximum(weights["bc_re"], 1.0)
 
         # logging
         if step % config.logging.log_interval == 0:
             evaluator(params, coeffs, weights, batch, step, ref_coords, ref_gt)
-
     # -----------------------------------------------
 
     # Save model in hydra output directory
     model_path = (
         hydra.core.hydra_config.HydraConfig.get().runtime.output_dir + "/model.eqx"
     )
-    bvp = BVPModel(model, transforms, config, params, None, coeffs)
+    bvp = BVPModel(model, config, transforms, params, None, coeffs)
     eqx.tree_serialise_leaves(model_path, bvp)
     logging.info(f"Model saved to {model_path}")
 
